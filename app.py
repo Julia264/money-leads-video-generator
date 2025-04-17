21386582ae6a064d2e3e073c5f1ed61000ce69d3
Julia Hany Fawzy <78325624+Julia264@users.noreply.github.com>
2025-04-17 19:29:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import torch
 import tempfile
 from moviepy import ImageSequenceClip
 import os
+import numpy 
 
 app = Flask(__name__, static_url_path='/static')
 CORS(app)
@@ -21,18 +22,21 @@ pipe.to("cuda" if torch.cuda.is_available() else "cpu")
 def index():
     return send_from_directory("static", "index.html")
 
+
 @app.route("/generate-video", methods=["POST"])
 def generate_video():
     image_file = request.files["image"]
     img = Image.open(image_file).convert("RGB")
     img = img.resize((384, 384))
 
-    video_frames = pipe(img, num_frames=6).frames[0]
+    # Convert the image to a numpy array
+    img_array = np.array(img)
+
+    video_frames = pipe(img_array, num_frames=6).frames[0]
 
     with tempfile.NamedTemporaryFile(delete=False, suffix=".mp4") as temp:
         clip = ImageSequenceClip(video_frames, fps=7)
-        clip.write_videofile(temp.name, codec="libx264", audio=False, verbose=False, logger=None)
-        return send_file(temp.name, mimetype="video/mp4", as_attachment=False)
-
+        clip.write_videofile(temp.name, codec="libx264", audio=False)
+        return send_file(temp.name, mimetype="video/mp4", as_attachment=True, download_name="output.mp4")
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
