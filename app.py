07c246a19d90681ab57cdf7d22aa7207825b2b6d
Julia Hany Fawzy <78325624+Julia264@users.noreply.github.com>
2025-04-22 03:22:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from diffusers import StableVideoDiffusionPipeline
 import torch
 import tempfile
 from moviepy import ImageSequenceClip, VideoFileClip
-from moviepy.video.fx import resize
+from moviepy.video.fx.resize import resize
 #from moviepy.video.fx.resize import resize
 
 import numpy as np
